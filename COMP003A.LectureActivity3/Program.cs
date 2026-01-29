cdb653bfbbce67aafb401d5fce1dbadf4af5a46e
gonzalez-7 <20agonzalezp@gmail.com>
2026-01-28 19:29:03 -0800

    Section A - Completed

--- COMP003A.LectureActivity3/Program.cs
+++ COMP003A.LectureActivity3/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Section A
+
+            Console.WriteLine("Section A - Academic Term Identification");
+
+            int currentMonth = DateTime.Now.Month;
+            Console.WriteLine($"Current Month: {currentMonth}");
+
+            if ( currentMonth >= 1 && currentMonth <= 5)
+            {
+                Console.WriteLine("Spring Term");
+            }
+            else if ( currentMonth == 6 || currentMonth == 7 )
+            {
+                Console.WriteLine("Summer Term");
+            }
+            else
+            {
+                Console.WriteLine("Fall Term");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(); // blank lines
+
+
         }
     }
 }
